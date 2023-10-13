9c93ab370fd440610bb7c4098c97c40bd02ac47a
Arseniy <arseniy1304@gmail.com>
2023-10-14 00:31:03 +0200

    grid update

--- frontend/MASSter-frontend/src/pages/grid.tsx
+++ frontend/MASSter-frontend/src/pages/grid.tsx
@@ -1,6 +1,10 @@
 import { FullscreenLoader } from "@/components/loaders";
+import { Card, CardTitle, CardFooter, CardDescription, CardContent } from "@/components/ui/card";
 import { pb } from "@/lib/pb-client";
+import { RecordModel } from "pocketbase";
 import { useQuery } from "react-query";
+import imgPlaceholder from "/src/assets/img/img-placeholder.webp";
+import { IconLoader3 } from "@tabler/icons-react";
 
 export function GridPage() {
     const listQuery = useQuery(
@@ -17,9 +21,47 @@ export function GridPage() {
         return <FullscreenLoader />;
     }
 
-    console.log(listQuery.data?.items)
+    console.log(listQuery.data?.items);
 
-    // pb.files.getUrl(/*item instance*/, "filename")
+    if (listQuery.data) {
+        if (listQuery.data.items.length != 0) {
+            const gridItems = [];
+            for (const item of listQuery.data?.items ?? []) {
+                gridItems.push(
+                    <GridCard item={item} key={item.id}></GridCard>
+                )
+            }
+            return <div className="grid grid-cols-4 gap-2">{gridItems}</div>;
+        } else {
+            return <div>No items!</div>
+        }
+    } else {
+        return <div>Error</div>
+    }
+}
 
-    return <div>grid</div>;
+function GridCard({item}: {item: RecordModel}) {
+    const fileQuery = useQuery(
+        [`get-file-${item.id}`],
+        () => {
+            return pb.files.getUrl(item, item.output_image[0]);
+        }
+    );
+
+    let elem = <IconLoader3 className="animate-spin"></IconLoader3>;
+    if (fileQuery.isSuccess) {
+        elem = <img className="rounded-lg h-52 w-full object-cover"
+            src={fileQuery.data.length == 0 ? imgPlaceholder : fileQuery.data}></img>
+    };
+
+    return (
+        <Card className="w-full relative">
+            <CardContent className="p-0">{elem}</CardContent>
+            <CardFooter className="rounded-lg flex w-full justify-between
+                absolute bottom-0 bg-gradient-to-b from-transparent to-black pt-8">
+                <CardTitle className="text-white">Image</CardTitle>
+                <CardDescription className="text-gray-300">{item.created}</CardDescription>
+            </CardFooter>
+        </Card>
+    );
 }
