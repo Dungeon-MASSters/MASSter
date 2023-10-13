eec2e86b83d869124cfe0f6ca58a63eb2ccc399f
Nikon Podgorny <podgorny.nikon@yandex.ru>
2023-10-13 21:29:15 +0300

    form example

--- frontend/MASSter-frontend/src/pages/main.tsx
+++ frontend/MASSter-frontend/src/pages/main.tsx
@@ -1,3 +1,48 @@
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type fields = {
+    something: string;
+};
+
 export function MainPage() {
-    return <div>this is main page</div>;
+    const form = useForm<fields>();
+    const onSubmit: SubmitHandler<fields> = (data) => {
+        console.log("submit", data);
+    };
+
+    return (
+        <div>
+            <h1 className="text-xl font-bold">Создать оформление для видео!</h1>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <FormField
+                        control={form.control}
+                        name="something"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Username</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="shadcn" {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    This is your public display name.
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </form>
+            </Form>
+        </div>
+    );
 }
