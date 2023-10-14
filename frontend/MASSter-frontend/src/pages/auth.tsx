7ff14512ac20d88f94994d1dd16cd70d18f311a3
Arseniy <arseniy1304@gmail.com>
2023-10-14 18:47:35 +0200

    nice auth

--- frontend/MASSter-frontend/src/pages/auth.tsx
+++ frontend/MASSter-frontend/src/pages/auth.tsx
@@ -4,6 +4,7 @@ import { pb } from "@/lib/pb-client";
 import { IconBrandVk, IconBrandYandex, IconLoader3 } from "@tabler/icons-react";
 import { RecordAuthResponse, RecordModel } from "pocketbase";
 import { useQuery } from "react-query";
+import samPic from '../assets/img/maxresdefault.jpg';
 
 function authWithVK() {
     return pb.collection("users").authWithOAuth2({ provider: "vk" });
@@ -35,47 +36,62 @@ export function AuthPage({ onSuccess, onError }: authPageProps) {
     );
 
     return (
-        <div className="flex justify-center p-4">
-            <Card>
-                <CardHeader>
-                    <CardTitle>Войти или зарегистрироваться</CardTitle>
-                </CardHeader>
+        <div className="flex absolute top-0 left-0 h-[100vh] w-[100vw] bg-black">
+            <div className="flex-grow relative h-full w-0">
+                <img src={samPic} className="h-full w-full object-cover"></img>
+                <div className="absolute top-0 left-0 h-full w-full bg-black opacity-40"></div>
+                <div className="absolute top-0 right-0 h-full w-full bg-gradient-to-r to-black from-transparent"></div>
+                <div className="absolute top-0 right-0 h-full w-full p-10">
+                    <div className="h-full w-full flex max-w-2xl justify-center items-center flex-col">
+                        <div className="text-white text-6xl text-center font-bold mb-5">Жестко насрали</div>
+                        <div className="text-gray-200 text-2xl text-center">Генерируйте арты к своим видео</div>
+                    </div>
+                </div>
+            </div>
+            <div className="max-w-lg px-20 h-full">
+                <div className="flex justify-center h-full p-4 items-center">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle className="text-center">Войти или зарегистрироваться</CardTitle>
+                        </CardHeader>
 
-                <CardContent className="gap-2 flex-col flex">
-                    <Button
-                        onClick={() => authVKQuery.refetch()}
-                        className="flex gap-1 w-full"
-                        disabled={
-                            authVKQuery.isFetching || authYandexQuery.isFetching
-                        }
-                    >
-                        <span>Войти через</span>
-                        <IconBrandVk strokeWidth="1.5" />
-                        {authVKQuery.isFetching && (
-                            <IconLoader3
-                                strokeWidth="1.5"
-                                className="animate-spin"
-                            />
-                        )}
-                    </Button>
-                    <Button
-                        onClick={() => authYandexQuery.refetch()}
-                        className="flex gap-1 w-full"
-                        disabled={
-                            authVKQuery.isFetching || authYandexQuery.isFetching
-                        }
-                    >
-                        <span>Войти через</span>
-                        <IconBrandYandex strokeWidth="1.5" />
-                        {authYandexQuery.isFetching && (
-                            <IconLoader3
-                                strokeWidth="1.5"
-                                className="animate-spin"
-                            />
-                        )}
-                    </Button>
-                </CardContent>
-            </Card>
+                        <CardContent className="gap-2 flex-col flex">
+                            <Button
+                                onClick={() => authVKQuery.refetch()}
+                                className="flex gap-1 w-full"
+                                disabled={
+                                    authVKQuery.isFetching || authYandexQuery.isFetching
+                                }
+                            >
+                                <span>Войти через</span>
+                                <IconBrandVk strokeWidth="1.5" />
+                                {authVKQuery.isFetching && (
+                                    <IconLoader3
+                                        strokeWidth="1.5"
+                                        className="animate-spin"
+                                    />
+                                )}
+                            </Button>
+                            <Button
+                                onClick={() => authYandexQuery.refetch()}
+                                className="flex gap-1 w-full"
+                                disabled={
+                                    authVKQuery.isFetching || authYandexQuery.isFetching
+                                }
+                            >
+                                <span>Войти через</span>
+                                <IconBrandYandex strokeWidth="1.5" />
+                                {authYandexQuery.isFetching && (
+                                    <IconLoader3
+                                        strokeWidth="1.5"
+                                        className="animate-spin"
+                                    />
+                                )}
+                            </Button>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
         </div>
     );
 }
